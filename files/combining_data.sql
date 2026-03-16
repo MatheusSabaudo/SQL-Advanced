741f12d49633589fa41c75d7ce3bb5341c52d989
Matheus Sabaudo Rodrigues <matteosabaudo@outlook.it>
2026-03-16 12:34:58 +0100

    Advanced Joins

--- files/combining_data.sql
+++ files/combining_data.sql
@@ -37,7 +37,6 @@ RIGHT JOIN orders o
 ON c.cust_id = o.cust_id
 
 -- Full Join
-
 SELECT
     c.cust_id,
     c.first_name,
@@ -47,3 +46,39 @@ SELECT
 FROM customers c
 FULL JOIN orders o
 ON c.cust_id = o.cust_id
+
+-- Left Anti Join
+SELECT
+    c.cust_id,
+    c.first_name,
+    c.country,
+    o.order_id,
+    o.order_status
+FROM customers c
+LEFT JOIN orders o
+ON c.cust_id = o.cust_id
+WHERE o.cust_id IS NULL
+
+-- Right Anti Join
+SELECT
+    c.cust_id,
+    c.first_name,
+    c.country,
+    o.order_id,
+    o.order_status
+FROM orders o
+RIGHT JOIN customers c
+ON c.cust_id = o.cust_id
+WHERE c.cust_id IS NULL
+
+-- Full Anti Join
+SELECT
+    c.cust_id,
+    c.first_name,
+    c.country,
+    o.order_id,
+    o.order_status
+FROM customers c
+FULL JOIN orders o
+ON c.cust_id = o.cust_id
+WHERE c.cust_id IS NULL OR o.cust_id IS NULL
